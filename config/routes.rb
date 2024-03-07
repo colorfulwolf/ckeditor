828bdb2db79ea3f2975d74cee94ebcee8a65da05
colorfulberry <senluowanxiangt@gmail.com>
2024-03-07 15:25:26 +0800

    fix: fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Ckeditor::Engine.routes.draw do
-  resources :pictures, only: [:index, :create, :destroy], defaults: { responseType: 'json' }
+  resources :pictures, only: [:index, :destroy]
+  post :pictures, to: 'pictures#create', defaults: { responseType: 'json' }
   resources :attachment_files, only: [:index, :create, :destroy]
 end
